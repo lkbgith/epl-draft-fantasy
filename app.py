0e277962b32719f941e85012a50f1ec331d75157
lkbgith <leighb101@gmail.com>
2025-06-23 13:00:55 +1000

    Drop down list fixed and showing in front of the display

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO, emit
 from werkzeug.utils import secure_filename
